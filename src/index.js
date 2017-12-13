910ecf612f709060713957455f7f65fb0ce1cfd3
Greg Linklater <greglink49@gmail.com>
2017-12-13 22:57:27 +0200

    feat(lib): export Application and abstract Command

---
+++ src/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+
+/**
+ * Dependencies
+ * @ignore
+ */
+const Application = require('./Application')
+const Command = require('./Command')
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports = {
+  Application,
+  Command,
+}
